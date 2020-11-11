a94ccf3f95be77ee77223c4b7fb142f8a1c35f10
sokkunnorn <sokkun.norn2019@gmail.com>
2020-11-11 19:58:12 +0700

    add local storage login

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -99,7 +99,8 @@ export default {
       });
 
       this.isLoading = false;
-      console.log(data);
+      localStorage.setItem("catchUser", data);
+      this.$router.push("/");
     }
   }
 };
